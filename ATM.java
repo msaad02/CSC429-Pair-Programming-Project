af25ea2535d0ec98833a54b20bfc29474e0f3532
Matthew Saad <msaad02@outlook.com>
2024-02-15 13:03:10 -0500

    Add launcher class for ATM

--- ATM.java
+++ ATM.java
@@ -2,6 +2,7 @@
 // specify the package
 
 // system imports
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.io.FileOutputStream;
@@ -33,64 +34,62 @@ import userinterface.MainStageContainer;
 import userinterface.WindowPosition;
 
 
-/** The class containing the main program  for the ATM application */
+/**
+ * The class containing the main program  for the ATM application
+ */
 //==============================================================
-public class ATM extends Application
-{
+public class ATM extends Application {
 
-	private Teller myTeller;		// the main behavior for the application
+    private Teller myTeller;        // the main behavior for the application
 
-	/** Main frame of the application */
-	private Stage mainStage;
+    /**
+     * Main frame of the application
+     */
+    private Stage mainStage;
 
 
-	// start method for this class, the main application object
-	//----------------------------------------------------------
-	public void start(Stage primaryStage)
-	{
-	   System.out.println("ATM Version 3.00");
-	   System.out.println("Copyright 2004/2015 Sandeep Mitra and T M Rao");
+    // start method for this class, the main application object
+    //----------------------------------------------------------
+    public void start(Stage primaryStage) {
+        System.out.println("ATM Version 3.00");
+        System.out.println("Copyright 2004/2015 Sandeep Mitra and T M Rao");
 
-           // Create the top-level container (main frame) and add contents to it.
-	   MainStageContainer.setStage(primaryStage, "Brockport Bank ATM Version 3.00");
-	   mainStage = MainStageContainer.getInstance();
+        // Create the top-level container (main frame) and add contents to it.
+        MainStageContainer.setStage(primaryStage, "Brockport Bank ATM Version 3.00");
+        mainStage = MainStageContainer.getInstance();
 
-	   // Finish setting up the stage (ENABLE THE GUI TO BE CLOSED USING THE TOP RIGHT
-	   // 'X' IN THE WINDOW), and show it.
-           mainStage.setOnCloseRequest(new EventHandler <javafx.stage.WindowEvent>() {
+        // Finish setting up the stage (ENABLE THE GUI TO BE CLOSED USING THE TOP RIGHT
+        // 'X' IN THE WINDOW), and show it.
+        mainStage.setOnCloseRequest(new EventHandler<javafx.stage.WindowEvent>() {
             @Override
             public void handle(javafx.stage.WindowEvent event) {
                 System.exit(0);
             }
-           });
+        });
 
-           try
-	   {
-		myTeller = new Teller();
-	   }
-	   catch(Exception exc)
-	   {
-		System.err.println("ATM.ATM - could not create Teller!");
-		new Event(Event.getLeafLevelClassName(this), "ATM.<init>", "Unable to create Teller object", Event.ERROR);
-		exc.printStackTrace();
-	   }
+        try {
+            myTeller = new Teller();
+        } catch (Exception exc) {
+            System.err.println("ATM.ATM - could not create Teller!");
+            new Event(Event.getLeafLevelClassName(this), "ATM.<init>", "Unable to create Teller object", Event.ERROR);
+            exc.printStackTrace();
+        }
 
 
-  	   WindowPosition.placeCenter(mainStage);
+        WindowPosition.placeCenter(mainStage);
 
-           mainStage.show();
-	}
+        mainStage.show();
+    }
 
 
-	/** 
-	 * The "main" entry point for the application. Carries out actions to
-	 * set up the application
-	 */
-	//----------------------------------------------------------
-    	public static void main(String[] args)
-	{
+    /**
+     * The "main" entry point for the application. Carries out actions to
+     * set up the application
+     */
+    //----------------------------------------------------------
+    public static void main(String[] args) {
 
-		launch(args);
-	}
+        launch(args);
+    }
 
 }
